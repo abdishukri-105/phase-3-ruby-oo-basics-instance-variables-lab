2e3786c2fe4be8ceb5fa874b6b098513f89056aa
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-09 15:26:52 +0300

    passed all test

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
+require "pry"
 class Dog
-
+#   setter method 
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    # getter methodadd.
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "lassie"
+lassie.name
+
+
+binding.pry
